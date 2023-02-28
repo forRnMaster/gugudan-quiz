cef2c864230488d12bfd004aebd256081c83f407
박성혜 <comet@bagseonghyeui-MacBookPro.local>
2023-02-28 17:24:42 +0900

    [Init] 뼈대 설정

---
+++ src/styles/colors.ts
@@ -0,0 +1,59 @@
+const Colors = {
+  blueberry: '#7344e5',
+  blueberryShade: '#33277a',
+  blueberryTone: '#4d3bb8',
+  blueberryTint: '#b3a7fa',
+
+  lemonZest: '#ffee44',
+  lemonZestShade: '#807722',
+  lemonZestTone: '#bfb333',
+  lemonZestTint: '#fff7a2',
+
+  red: '#ff3700',
+  redShade: '#801c00',
+  redTone: 'bf2900',
+  redTint: '#ff9b80',
+
+  coral: '#ff896f',
+  coralShade: '#804538',
+  coralTone: '#bf6753',
+  coralTint: '#ffc4b7',
+
+  turquoise: '#28d0d5',
+  turquoiseShade: '#14686a',
+  turquoiseTone: '#1e9ca0',
+  turquoiseTint: '#93e7ea',
+
+  skyblue: '#49d1fc',
+  skyblueShade: '#24687e',
+  skyblueTone: '#379dbd',
+  skyblueTint: '#a4e8fe',
+
+  bk01: '#0a0b0c',
+  bk02: '#18161d',
+  bk03: '#2a2c3d',
+  bk04: '#272430',
+  bk05: '#2a2731',
+  bk06: '#2c2a30',
+  bk07: '#000000',
+
+  white01: '#ffffff',
+
+  white01Opacity20: '#ffffff33',
+  white01Opacity80: '#ffffffcc',
+
+  text01: '#0a0b0c',
+  text02: '#3c444b',
+  text03: '#61676e',
+  text04: '#ced4d9',
+  text05: '#ffffff',
+
+  button01: '#404c5a',
+  button02: '#ced4d9',
+  button03: '#d7dadc',
+  button04: '#f0f0f3',
+  button05: '#f7f8f8',
+  button06: '#ffffff',
+} as const;
+
+export default Colors;
